c6526908af1fe47a2417a2cced617ecdf0a76527
compilemedaddy <compilemedaddy@users.noreply.github.com>
2020-08-25 08:07:47 +0200

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿SELECT * FROM Recipe
+
+SELECT * FROM Ingredient
+
+SELECT a.Name FROM Ingredient a
+INNER JOIN RecipeIngredient b on a.Id = b.IngredientId
+WHERE b.RecipeId = 1;
+
+UPDATE Recipe
+SET Name = 'Salad'
+Where Id = 1;
+
+INSERT INTO Recipe
+VALUES ('Salad Deluxe', 50, 'Combine what you want');
+
+DELETE FROM Recipe
+WHERE id = 4;
